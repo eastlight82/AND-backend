3ed7b186ecea1a049219f3aa3def2583d3e2edd8
eastlight82 <bigstake82@gmail.com>
2025-08-03 16:40:25 +0900

    Feat Project

---
+++ src/test/java/com/example/and_backend/AndBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.and_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
