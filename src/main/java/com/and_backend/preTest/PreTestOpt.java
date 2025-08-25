724f32cec819f8208eaf592fc154aa8a24dcfbd5
eastlight82 <bigstake82@gmail.com>
2025-08-25 17:41:37 +0900

    fix PreTestOpt (add @getter)

--- src/main/java/com/and_backend/preTest/PreTestOpt.java
+++ src/main/java/com/and_backend/preTest/PreTestOpt.java
@@ -1,8 +1,10 @@
 package com.and_backend.preTest;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity @Table(name = "preTestOpt")
+@Data
 public class PreTestOpt {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long preTestOptId;
